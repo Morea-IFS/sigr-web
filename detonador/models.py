a9d8b56a334d85282ab1c104a219db18c9bf267b
victor <vicfla1981@gmail.com>
2026-04-24 14:51:19 -0300

    🐛 resolving detonator bug

--- detonador/models.py
+++ detonador/models.py
@@ -4,7 +4,7 @@ from app.models import Device
 
 class DetonadorEvento(models.Model):
     devices = models.ManyToManyField(Device)   
-    name = models.CharField(max_length=100, default="Meu Evento")
+    name = models.CharField(max_length=100, default="")
     delay_ms = models.PositiveIntegerField(default=1000, help_text="Intervalo em milissegundos entre os acionamentos")
     pin_sequence = models.TextField(help_text="Sequência de Pinos, separados por vírgula. Ex: 5,7,1")
     is_active = models.BooleanField(default=False)
